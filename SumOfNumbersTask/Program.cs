e8ace2ed81d0f97bafc883d789d9637dae84539b
Aleksey_Filkin <acid_wind@mail.ru>
2023-02-05 14:54:15 +0700

    SumOfNumbersTask solved

--- SumOfNumbersTask/Program.cs
+++ SumOfNumbersTask/Program.cs
@@ -1,1 +1,9 @@
 ﻿//Найти сумму чисел от 1 до А
+Console.Write("Введите последнее число А: ");
+int A = int.Parse(Console.ReadLine() ?? "");
+int result = 0;
+for (int i = 1; i <= A; i++)
+{
+    result = i + result;
+}
+Console.Write($"Сумма всех чисел от 1 до {A} равна: {result}");
