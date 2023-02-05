2e2f42d79626b495700c546f289c6a9620dd7bd4
Aleksey_Filkin <acid_wind@mail.ru>
2023-02-05 14:43:47 +0700

    CubesOfNumbersTask solved

--- CubesOfNumbersTask/Program.cs
+++ CubesOfNumbersTask/Program.cs
@@ -1,1 +1,9 @@
 ﻿//Найти кубы чисел от 1 до N
+Console.Write("Введите последнее число последовательности N: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+for (int i = 1; i <= N; i++)
+{
+    double result = Math.Pow(i, 3);
+    //int result = i * i * i;
+    Console.WriteLine($"{i}^3 = {result}");
+}
