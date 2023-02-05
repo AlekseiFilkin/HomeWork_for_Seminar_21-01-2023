a7241c71aff9855cbb48191261fbf319b3aec4c0
Aleksey_Filkin <acid_wind@mail.ru>
2023-02-05 15:09:04 +0700

    DegreeOfNumberTask folder adde

--- DegreeOfNumberTask/Program.cs
+++ DegreeOfNumberTask/Program.cs
@@ -1,1 +1,10 @@
 ﻿//Возведите число А в натуральную степень B используя цикл
+Console.Write("Введите число А: ");
+int a = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите число степени В: ");
+int b = int.Parse(Console.ReadLine() ?? "");
+for (int i = 1; i <= b; i++)
+{
+    double result = Math.Pow(a, i);
+    Console.WriteLine($"{a}^{i} = {result}");
+}
