85f6d537b20f93a0dc30c44caef0f09362cafa17
Aleksey_Filkin <acid_wind@mail.ru>
2023-02-11 15:47:02 +0700

    SegmentInArrayTask solved

--- SegmentInArrayTask/Program.cs
+++ SegmentInArrayTask/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿//В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+int[] array = new int[123];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 100);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] >= 10 && array[i] <= 99)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество элементов отрезка [10, 99] = {count}");
