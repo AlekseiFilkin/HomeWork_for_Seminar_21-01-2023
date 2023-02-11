5d5eb941f36f88b8924f5dcf6d69ded5b47a9a99
Aleksey_Filkin <acid_wind@mail.ru>
2023-02-11 10:36:30 +0700

    SumOfNumInNumbTask solved

--- SumOfNumInNumbTask/Program.cs
+++ SumOfNumInNumbTask/Program.cs
@@ -1,1 +1,11 @@
 ﻿//Подсчитать сумму цифр в числе
+Console.Write("Введите число: ");
+int Num = int.Parse(Console.ReadLine() ?? "");
+int result = 0;
+while (Num != 0)
+{
+	int n = Num%10;
+	Num = Num/10;
+	result = result+n;
+}
+Console.WriteLine($"Сумма цифр в заданном числе равна {result}");
