40c69d9fac1af5a65f5ce54c88ffa3cca4b71fd9
Aleksey_Filkin <acid_wind@mail.ru>
2023-02-11 11:25:08 +0700

    CubeOfEvenNumbersTask solved

--- CubeOfEvenNumbersTask/Program.cs
+++ CubeOfEvenNumbersTask/Program.cs
@@ -1,1 +1,10 @@
 ﻿//Показать кубы чисел, заканчивающихся на четную цифру
+int N = new Random().Next(1, 50);
+int Num = 1;
+Console.WriteLine($"Кубы чисел, заканчивающихся на четную цифру в последовательности от 1 до {N}: ");
+while (Num <= N)
+{
+    var result = Math.Pow(Num, 3);
+    if (result % 2 == 0) Console.WriteLine($"{Num}^3 = {result}");
+    Num++;
+}
