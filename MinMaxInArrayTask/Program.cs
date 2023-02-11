4600571f1a36206b4c7db0748d1d706158e7b33b
Aleksey_Filkin <acid_wind@mail.ru>
2023-02-11 17:35:25 +0700

    MinMaxInArrayTask solved

--- MinMaxInArrayTask/Program.cs
+++ MinMaxInArrayTask/Program.cs
@@ -1,1 +1,25 @@
 ﻿//В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+Console.Write("Введите размерность массива: ");
+int n = int.Parse(Console.ReadLine() ?? "");
+double max = 0;
+double min = 1;
+double result;
+double[] array = new double[n];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().NextDouble();
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        max = array[i];
+    }
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < min)
+    min = array[i];
+}
+result = max - min;
+Console.WriteLine($"Разница между {max} и {min} равна {result}");
