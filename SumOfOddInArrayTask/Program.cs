10101f58c4dd59c1d247bb4aeb654b650bfa138a
Aleksey_Filkin <acid_wind@mail.ru>
2023-02-11 16:15:17 +0700

    SumOfOddInArrayTask solved

--- SumOfOddInArrayTask/Program.cs
+++ SumOfOddInArrayTask/Program.cs
@@ -1,1 +1,17 @@
-﻿//
+﻿//Найти сумму чисел одномерного массива стоящих на нечетной позиции
+Console.Write("Введите размерность массива: ");
+int n = int.Parse(Console.ReadLine() ?? "");
+int sum = 0;
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+        Console.Write($"{array[i]} ");
+    }
+Console.WriteLine();    
+for (int i = 1; i < array.Length; i++)
+{
+    sum+=array[i];
+    i+=1; 
+} 
+Console.Write($"Сумма нечетных элементов массива равна {sum}");
