9a5ecb047203dbd8878b5953a45ca37289fd6590
Aleksey_Filkin <acid_wind@mail.ru>
2023-02-11 12:17:20 +0700

    ArrayWithSomeNumberTask solved

--- ArrayWithSomeNumberTask/Program.cs
+++ ArrayWithSomeNumberTask/Program.cs
@@ -1,1 +1,24 @@
-﻿//Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+﻿//Определить, присутствует ли в заданном массиве, некоторое число
+Console.Write("Введите размерность массива: ");
+int n = int.Parse(Console.ReadLine() ?? "");
+//Console.WriteLine("Введите число для поиска в массиве: ");
+//int Num = int.Parse(Console.ReadLine() ?? "");
+bool res = false;
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 90);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+Console.WriteLine("Введите число для поиска в массиве: ");
+int Num = int.Parse(Console.ReadLine() ?? "");
+foreach (int k in array)
+{
+    if (k == Num)
+        res = true;
+}
+if (res)
+    Console.Write($"Число {Num} присутствует в массиве");
+else
+    Console.Write($"Числа {Num} нет в массиве");
