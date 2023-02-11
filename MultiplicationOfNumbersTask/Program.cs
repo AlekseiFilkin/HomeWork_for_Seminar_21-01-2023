bbcea4027a29cd55e2dd369b95344a6acb7426e0
Aleksey_Filkin <acid_wind@mail.ru>
2023-02-11 10:55:42 +0700

    multiplicationfumbersask solved

--- MultiplicationOfNumbersTask/Program.cs
+++ MultiplicationOfNumbersTask/Program.cs
@@ -1,1 +1,9 @@
 ﻿//Написать программу вычисления произведения чисел от 1 до N
+Console.Write("Введите последнее число N: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+int result = 1;
+for (int i = 1; i <= N; i++)
+    {
+        result *= i;
+    }
+Console.Write($"Произведение чисел от 1 до {N} равно {result}");
